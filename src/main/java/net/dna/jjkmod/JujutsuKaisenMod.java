76fa72647a08dd376fa61f6a7569c9cfe93f80e1
Noah A <noah.ahmed.278@gmail.com>
2023-09-09 13:28:27 -0500

    Attempting Custom Quest Addition

--- src/main/java/net/dna/jjkmod/JujutsuKaisenMod.java
+++ src/main/java/net/dna/jjkmod/JujutsuKaisenMod.java
@@ -1,6 +1,8 @@
 package net.dna.jjkmod;
 
 import com.mojang.logging.LogUtils;
+import dev.zanckor.api.filemanager.quest.register.LoadQuest;
+import dev.zanckor.mod.QuestApiMain;
 import net.dna.jjkmod.item.ModItems;
 import net.dna.jjkmod.networking.ModMessages;
 import net.minecraft.client.Minecraft;
@@ -9,6 +11,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,6 +20,9 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
+
+import java.io.IOException;
+
 //commit test
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(JujutsuKaisenMod.MOD_ID)
@@ -44,6 +50,7 @@ public class JujutsuKaisenMod
 
     }
 
+
     private void commonSetup(final FMLCommonSetupEvent event)
     {
         event.enqueueWork(() -> {
@@ -70,5 +77,10 @@ public class JujutsuKaisenMod
         {
 
         }
+    }
+    @SubscribeEvent
+    public static void registerTemplates(ServerAboutToStartEvent e)throws IOException{
+        JujutsuKaisenMod.LOGGER.debug("This should be called");
+        LoadQuest.registerQuest(e.getServer(), JujutsuKaisenMod.MOD_ID);
     }
 }
---
+++ src/main/java/net/dna/jjkmod/util/KeyBinding.java
@@ -0,0 +1,6 @@
+package net.dna.jjkmod.util;
+
+public class KeyBinding {
+    public static final String KEY_CATEGORY_JJKMOD = "key.category.jjkmod.jjk";
+
+}
